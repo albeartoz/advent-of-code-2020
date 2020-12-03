d72e0e4b18f18212baff97d612e8168e01c8562b
albeartoz <zhang.alb@husky.neu.edu>
2020-12-03 00:32:04 -0500

    day 2 part 2

--- day2/cpp/main.cpp
+++ day2/cpp/main.cpp
@@ -2,19 +2,34 @@
 #include <string>
 #include <algorithm>
 
-int main(void) {
-int min;
-int max;
-char trash;
-char c;
-std::string pwd;
-int valid = 0;
-while (std::cin >> min >> trash >> max >> c >> trash >> pwd) {
-int freq = std::count(pwd.begin(), pwd.end(), c);
-if (freq >= min && freq <= max) {
-++valid;
-}
-}
-std::cout << valid << std::endl;
-return 0;
+int main(void)
+{
+    int min;
+    int max;
+    char trash;
+    char c;
+    std::string pwd;
+    int valid = 0;
+    while (std::cin >> min >> trash >> max >> c >> trash >> pwd)
+    {
+        /*
+        int freq = std::count(pwd.begin(), pwd.end(), c);
+        
+        if (freq >= min && freq <= max)
+        {
+            ++valid;
+        }
+        */
+        if (min <= pwd.size() && max <= pwd.size())
+        {
+            char minchar = pwd.at(min - 1);
+            char maxchar = pwd.at(max - 1);
+            if ((minchar == c || maxchar == c) && !(minchar == c && maxchar == c))
+            {
+                ++valid;
+            }
+        }
+    }
+    std::cout << valid << std::endl;
+    return 0;
 }
